da29ada4aaa8f3572ca552b7a56c63ba1a2329b0
Robert Shockley <rshockle@gmail.com>
2023-01-28 22:21:52 -0500

    Add logging from the old file

--- ben.py
+++ ben.py
@@ -3,13 +3,32 @@ import discord
 from discord.ext import commands
 import openai
 from dotenv import load_dotenv
+import logging
 
+
+# Logging rec from the docs
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+handler_console = logging.StreamHandler()
+handler_console.setLevel(logging.INFO)
+logger.addHandler(handler_console)
+
+# Load all the environment variables using ".env"
 load_dotenv()
 
+# OpenAI key from dotenv
 openai.api_key=os.environ.get("OPENAI_KEY")
 
+# Intents are what all the bot is allowed to do via privileged gateway
 intents = discord.Intents.default()
 intents.message_content = True
+
+#bot commands start with '!'
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
@@ -30,17 +49,6 @@ async def benDraw(ctx):
             )
     await ctx.send(image.data[0].url)
 
-# @bot.command()
-# async def benQuip(ctx):
-#     completion = openai.Completion.create(
-#             model="text-davinci-003",
-#             max_tokens=20,
-#             temperature=0.5,
-#             top_p=0.3,
-#             frequency_penalty=0.5,
-#             presence_penalty=0.0,
-#             prompt=ctx.message.content
-#             )
-#     await ctx.send(completion.choices[0].text)
-
+# Discord bot token from dotenv
+logger.info('Starting bot')
 bot.run(os.environ.get("TOKEN"))
